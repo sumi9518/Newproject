29b370be8a156014e93c924b1fe28fdace4f887e
sumanyadav9518@gmail.com <sumanyadav9518@gmail.com>
2019-09-17 16:03:28 +0200

    updated css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+      <div className="container">
+        <div className="header">
+          <div className="logo">
+            <Link to="/index">
+            GoFooddy
+            </Link>
+          </div>
+          <div className="navigation">
+            <ul className="nav">
+              <li>Home |</li>
+              <li>About |</li>
+              <li>Recipes </li>
+            </ul>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="content">
+          <div className="content-header">Recipes</div>
+          <ul className="recipes">
+            <li>Vegan</li>
+            <li>Vegetarian</li>
+            <li>Non Vegeterian</li>
+          </ul>
+        </div>
+        <div className="footer">
+            <p>&copy; 2019</p>
+        </div>
+        
       </div>
+      </Router>
     );
   }
 }
